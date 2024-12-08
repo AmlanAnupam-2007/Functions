67df9d7c604ec05e5ba7d2ac0a9ea5fd31b5d9e3
AmlanAnupam-2007 <amlananupam2007@gmail.com>
2024-12-08 19:21:11 +0530

    functions

---
+++ Parametric(factorial).py
@@ -0,0 +1,9 @@
+def para(n):
+    f=1
+    for i in range(1,n+1):
+        f=f*i
+    print(f)
+para(5)
+
+
+    
---
+++ return type.py
@@ -0,0 +1,21 @@
+def Product(a,b):
+    return a*b
+print(Product(4,3))
+
+def Modulus(a,b):
+    return a%b
+print(Modulus(4,2))
+
+def Floor_Division(a,b):
+    return a//b
+print(Floor_Division(4,2))
+
+def Exponents(a):
+    return a**4
+print(Exponents(2))
+
+def Division(a,b):
+    return a/b
+x=int(input("Enter the number"))
+y=int(input("Enter the other number"))
+print(Division(x,y))
